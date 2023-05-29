da155a9e63979740874872e592554080c88eeda9
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-05-29 18:00:16 +0700

    Get match API

--- src/containers/Match/Match.jsx
+++ src/containers/Match/Match.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getMatchList from '../../apis/matchApi';
 
 const Match = () => {
-  return <div>Match</div>;
+  const [rows, setRows] = useState([]);
+  const fetchData = async () => {
+    const data = await getMatchList();
+    console.log(data);
+    console.log(Object.values(data));
+    setRows(Object.values(data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {rows.map((row) => (
+        <div>
+          {row.map((sub, key) => (
+            <div>
+              {key}
+              {sub.date}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Match;
