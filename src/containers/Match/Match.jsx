66a87941976b0d23211e96a77432516741f5e0ea
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-06-01 16:17:15 +0700

    Create match interface frame

--- src/containers/Match/Match.jsx
+++ src/containers/Match/Match.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import getMatchList from '../../apis/matchApi';
+import { MatchStyle, Image } from './index.style';
 
 const Match = () => {
-  const [matchs, setMatchs] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [dates, setDates] = useState([]);
+
   const fetchData = async () => {
     const data = await getMatchList();
-    setMatchs(Object.values(data));
+    setMatches(Object.values(data));
+    setDates(Object.keys(data));
   };
 
   useEffect(() => {
@@ -13,18 +17,70 @@ const Match = () => {
   }, []);
 
   return (
-    <div>
-      {matchs.map((match) => (
-        <div>
-          {match.map((subMatch, key) => (
-            <div>
-              {key}
-              {subMatch.date}
+    <MatchStyle>
+      {matches.map((match, index) => (
+        <div className="matchComponent">
+          <form>
+            <label className="dateTime" htmlFor={index}>
+              {dates[index]}
+            </label>
+            <input
+              name="hidden"
+              id={index}
+              className="hidden"
+              type="checkbox"
+              hidden
+            />
+            <div className="matches">
+              {match.map((subMatch) => (
+                <div className="match">
+                  <div className="subMatch">
+                    <div className="title">
+                      <p>{subMatch.groupStage}</p>
+                    </div>
+                    <div className="info">
+                      <div className="twoTeam">
+                        <div className="team">
+                          <Image
+                            src={subMatch.imagehomeTeam.replace(
+                              '{format}-{size}',
+                              'sq-1',
+                            )}
+                          />
+                          <p className="name"> {subMatch.homeTeam}</p>
+                        </div>
+                        <div className="dateTable">
+                          <div className="column">
+                            <div className="table">{subMatch.table}</div>
+                            <div className="highLight">
+                              {subMatch.timehighlight}
+                            </div>
+                          </div>
+                        </div>
+                        <div className="team">
+                          <Image
+                            src={subMatch.imageawayTeam.replace(
+                              '{format}-{size}',
+                              'sq-1',
+                            )}
+                          />
+                          <p className="name"> {subMatch.awayTeam}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="result">
+                      <a href={subMatch.video} className="video">
+                        Video
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </form>
         </div>
       ))}
-    </div>
+    </MatchStyle>
   );
 };
 
---
+++ src/containers/Match/MatchDetail/MatchDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MatchDetailStyle } from './index.style';
+
+const MatchDetail = () => {
+  return (
+    <MatchDetailStyle>
+      <div className="matchDetail">hehe</div>
+    </MatchDetailStyle>
+  );
+};
+
+export default MatchDetail;
