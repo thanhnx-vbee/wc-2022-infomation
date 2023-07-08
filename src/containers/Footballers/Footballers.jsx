af3812e195762811590d7bcf279316a5824a89c1
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-07-08 23:00:35 +0700

    Create footballer frame

--- src/containers/Footballers/Footballers.jsx
+++ src/containers/Footballers/Footballers.jsx
@@ -16,7 +16,23 @@ const Footballers = () => {
   return (
     <FootballerStyle>
       {footballer.map((player) => (
-        <div className="players">{player.playerName}</div>
+        <div className="players">
+          <a href={player.information}>
+            <div className="playerInfor">
+              <img
+                src={player.playerPicture}
+                alt="/"
+                className="playerPicture"
+              />
+              <p className="name">{player.playerName}</p>
+            </div>
+            <div className="position">{player.position}</div>
+            <div className="nation">
+              <img src={player.nationImage} alt="/" className="nationImage" />
+              <p className="location">{player.nation}</p>
+            </div>
+          </a>
+        </div>
       ))}
     </FootballerStyle>
   );
--- src/containers/Footballers/index.style.js
+++ src/containers/Footballers/index.style.js
@@ -4,9 +4,53 @@ const FootballerStyle = styled.div`
   display: flex;
   margin-top: 80px;
   margin-bottom: 50px;
-  flex-direction: column;
+  justify-content: center;
+  align-items: baseline;
+  align-content: stretch;
+  flex-wrap: wrap;
   .players {
-    display: flex;
+    width: 10em;
+    justify-content: center;
+    flex-direction: column;
+    border: 2px solid #ccc;
+    height: auto;
+    border-radius: 1em;
+    margin-right: 1em;
+    margin-top: 0;
+    margin-bottom: 1em;
+    a {
+      margin-top: 0;
+      text-decoration: none;
+      font-size: 16px;
+      font-weight: 600;
+      color: #000;
+      .playerInfor {
+        border-radius: 1em;
+        .playerPicture {
+          width: 10em;
+          height: 240px;
+        }
+        .name {
+          margin: 0 1em;
+        }
+      }
+      .position {
+        margin-left: 1em;
+        margin-bottom: 0.1em;
+      }
+      .nation {
+        display: flex;
+        .nationImage {
+          width: 3em;
+          height: 2em;
+          margin-left: 1em;
+        }
+        .location {
+          margin-left: 0.5em;
+          margin-top: 0.2em;
+        }
+      }
+    }
   }
 `;
 
