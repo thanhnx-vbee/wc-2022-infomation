9f3d5e7de478a4d059d8af9f119bd5517298f024
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-07-15 14:37:42 +0700

    Fix layout responsive

--- src/containers/Layout/Footer/index.style.js
+++ src/containers/Layout/Footer/index.style.js
@@ -1,46 +1,109 @@
 import styled from 'styled-components';
 
 const FooterStyle = styled.div`
-  height: 100px;
-  background-color: #000000;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  .footerLeft {
-    flex: 1;
+  --mainColor: #09ae3e;
+  background-color: #000;
+  color: #fff;
+  li {
+    list-style: none;
+  }
+  .footerContainer {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
     align-items: center;
-    font-size: 16px;
-    font-weight: 300;
-    margin-left: 50px;
-    .footerLeftContent {
-      margin: 0px 0px 0px 25px;
+    .constact {
+      margin-left: 3em;
+      margin-bottom: 1.125rem;
+      .logo {
+        height: 35px;
+        width: auto;
+      }
+      .socialMedia {
+        margin-top: 0.5em;
+        display: flex;
+        .icon {
+          width: 30px;
+          height: auto;
+          margin-left: 0.5em;
+          cursor: pointer;
+        }
+      }
+    }
+    .list {
+      display: flex;
+      flex-direction: column;
+      gap: 0.75rem;
+      margin-bottom: 2em;
+      li {
+        h3 {
+          color: var(--mainColor);
+        }
+        color: #a8a29e;
+        transition: all 0.4s;
+        cursor: pointer;
+        &:hover {
+          color: var(--mainColor);
+        }
+      }
+    }
+    .subscribe {
+      margin-right: 3em;
+      margin-bottom: 1.125rem;
+      h3 {
+        color: var(--mainColor);
+      }
+      p {
+        color: #a8a29e;
+      }
+      .input {
+        display: flex;
+        align-items: center;
+        background-color: #171717;
+        border-radius: 4px;
+        input {
+          all: unset;
+          padding: 0.5rem;
+          width: 100%;
+        }
+        button {
+          background-color: var(--mainColor);
+          border: none;
+          padding: 0.5rem 1rem;
+          font-size: 1.125rem;
+          height: 100%;
+          border-radius: 0 4px 4px 0;
+          cursor: pointer;
+          align-items: center;
+          &:hover {
+            opacity: 0.8;
+          }
+        }
+      }
     }
   }
-  .footerContent {
-    flex: 2;
-    margin: 0px;
-    padding: 0px;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    font-size: 16px;
-    font-weight: 700;
-    color: #ffffff;
-    margin-right: 30px;
+  @media (max-width: 1024px) {
+    .footerContainer {
+      grid-template-columns: repeat(3, 1fr);
+      grap: 2rem;
+      .subscribe {
+        margin-left: 3rem;
+      }
+    }
   }
-  .footerRight {
-    display: flex;
-    flex: 6;
-    font-weight: 300;
-    font-size: 16px;
-    margin-right: 50px;
+
+  @media (max-width: 768px) {
+    .footerContainer {
+      grid-template-columns: repeat(2, 1fr);
+      grap: 2rem;
+    }
+  }
+
+  @media (max-width: 640px) {
+    .footerContainer {
+      grid-template-columns: repeat(1, 1fr);
+      padding: 3rem 2rem;
+    }
   }
 `;
 
-const LogoFooter = styled.img`
-  weight: auto;
-  height: 40px;
-`;
-
-export { FooterStyle, LogoFooter };
+export { FooterStyle };
--- src/containers/Layout/TopNav/TopNav.jsx
+++ src/containers/Layout/TopNav/TopNav.jsx
@@ -1,85 +1,133 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from 'react';
-import { Wrapper, Logo, MenuItem, Avatar, SelectList } from './index.style';
-import LogoImg from '../img/Slice_9.png';
+import { TopNavStyle } from './index.style';
+import TopNavLogo from '../img/Slice_9.png';
 import AvatarImg from '../img/thu_1.png';
 import { TABS } from '../../../constants/index';
 
 const TopNav = ({ tab, setTab }) => {
   const [value, setValue] = useState('en');
+  const [down, setDown] = useState(false);
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
-    <Wrapper>
-      <div className="appBar">
-        <div className="toolbar">
-          <div className="headLogo">
-            <Logo src={LogoImg} />
-          </div>
-          <div className="listItem">
-            <div className={`container ${tab === TABS.HOME ? 'active' : null}`}>
-              <div
-                onClick={() => {
-                  setTab(TABS.HOME);
-                }}
-              >
-                MATCH
-              </div>
-            </div>
-            <div
-              className={`container ${
-                tab === TABS.FOOTBALLERS ? 'active' : null
-              }`}
-            >
-              <div
-                onClick={() => {
-                  setTab(TABS.FOOTBALLERS);
-                }}
-              >
-                FOOTBALLERS
-              </div>
-            </div>
-            <div
-              className={`container ${
-                tab === TABS.STATISTICS ? 'active' : null
-              }`}
-            >
-              <div
-                className="subItem"
-                onClick={() => {
-                  setTab(TABS.STATISTICS);
-                }}
-              >
-                STATISTICS
-              </div>
-            </div>
-            <div
-              className={`container ${tab === TABS.RANKS ? 'active' : null}`}
-            >
-              <div
-                onClick={() => {
-                  setTab(TABS.RANKS);
-                }}
-              >
-                RANKS
-              </div>
-            </div>
-          </div>
-          <div className="headRight">
-            <SelectList value={value} onChange={handleChange}>
-              <MenuItem value="en" selected="selected">
-                ENGLISH(UK)
-              </MenuItem>
-              <MenuItem value="vi">VIETNAMESE</MenuItem>
-            </SelectList>
-            <Avatar src={AvatarImg} variant="rounded" />
-          </div>
-        </div>
+    <TopNavStyle>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+      />
+      <div className="TopRight">
+        <img className="logo" src={TopNavLogo} alt="/" />
       </div>
-    </Wrapper>
+      <ul className="listItem">
+        <li className={`container ${tab === TABS.HOME ? 'active' : null}`}>
+          <div
+            onClick={() => {
+              setTab(TABS.HOME);
+            }}
+          >
+            MATCH
+          </div>
+        </li>
+        <li
+          className={`container ${tab === TABS.FOOTBALLERS ? 'active' : null}`}
+        >
+          <div
+            onClick={() => {
+              setTab(TABS.FOOTBALLERS);
+            }}
+          >
+            FOOTBALLERS
+          </div>
+        </li>
+        <li
+          className={`container ${tab === TABS.STATISTICS ? 'active' : null}`}
+        >
+          <div
+            className="subItem"
+            onClick={() => {
+              setTab(TABS.STATISTICS);
+            }}
+          >
+            STATISTICS
+          </div>
+        </li>
+        <li className={`container ${tab === TABS.RANKS ? 'active' : null}`}>
+          <div
+            onClick={() => {
+              setTab(TABS.RANKS);
+            }}
+          >
+            RANKS
+          </div>
+        </li>
+      </ul>
+      <div className="TopLeft">
+        <select className="selectList" value={value} onChange={handleChange}>
+          <option className="menuItem" value="en" selected="selected">
+            ENGLISH (UK)
+          </option>
+          <option className="menuItem" value="vi">
+            VIETNAMESE
+          </option>
+        </select>
+        <img className="avatar" src={AvatarImg} variant="rounded" alt="/" />
+      </div>
+      <div
+        className="toggleBtn"
+        value="down"
+        onClick={() => setDown((prev) => !prev)}
+      >
+        <span className="material-symbols-outlined">menu</span>
+      </div>
+      <div className={down === true ? 'dropDown active' : 'dropDown'}>
+        <li className={`container ${tab === TABS.HOME ? 'active' : null}`}>
+          <div
+            onClick={() => {
+              setTab(TABS.HOME);
+            }}
+          >
+            MATCH
+          </div>
+        </li>
+        <li
+          className={`container ${tab === TABS.FOOTBALLERS ? 'active' : null}`}
+        >
+          <div
+            onClick={() => {
+              setTab(TABS.FOOTBALLERS);
+            }}
+          >
+            FOOTBALLERS
+          </div>
+        </li>
+        <li
+          className={`container ${tab === TABS.STATISTICS ? 'active' : null}`}
+        >
+          <div
+            className="subItem"
+            onClick={() => {
+              setTab(TABS.STATISTICS);
+            }}
+          >
+            STATISTICS
+          </div>
+        </li>
+        <li className={`container ${tab === TABS.RANKS ? 'active' : null}`}>
+          <div
+            onClick={() => {
+              setTab(TABS.RANKS);
+            }}
+          >
+            RANKS
+          </div>
+        </li>
+      </div>
+    </TopNavStyle>
   );
 };
 
