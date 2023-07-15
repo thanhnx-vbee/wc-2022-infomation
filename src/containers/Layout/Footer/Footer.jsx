4dbbacbe03986871a237e2d949ebbf10550a92f6
ThuKuRu <101419174+ThuKuRu@users.noreply.github.com>
2023-07-15 14:37:42 +0700

    Fix layout responsive

--- src/containers/Layout/Footer/Footer.jsx
+++ src/containers/Layout/Footer/Footer.jsx
@@ -1,37 +1,55 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
-import { FooterStyle, LogoFooter } from './index.style';
+import { FooterStyle } from './index.style';
 import FooterLogo from '../img/FooterLogo.png';
+import Facebook from '../img/fb.png';
+import Instagram from '../img/insta.png';
+import Twitter from '../img/twitter_1.png';
 
 const Footer = () => {
   return (
-    <div>
-      <FooterStyle>
-        <div className="footerLeft">
-          <LogoFooter src={FooterLogo} />
-          <div className="footerLeftContent">Copyright © 2023 by FOOTBALL</div>
+    <FooterStyle>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
+      <div className="footerContainer">
+        <div className="constact">
+          <img className="logo" src={FooterLogo} alt="/" />
+          <div className="socialMedia">
+            <img className="icon" src={Facebook} alt="/" />
+            <img className="icon" src={Instagram} alt="/" />
+            <img className="icon" src={Twitter} alt="/" />
+          </div>
         </div>
-        <div className="footerContent">
-          <ul>
-            <li>Về chúng tôi</li>
-            <li>Chính sách</li>
-            <li>Điều khoản</li>
-          </ul>
-          <ul>
-            <li>Tin tức</li>
-            <li>Liên hệ</li>
-            <li>Thành viên</li>
-          </ul>
+        <ul className="list">
+          <li>
+            <h3>Blog</h3>
+          </li>
+          <li>Tech</li>
+          <li>Adventures</li>
+          <li>Music</li>
+        </ul>
+        <ul className="list">
+          <li>
+            <h3>Product</h3>
+          </li>
+          <li>App</li>
+          <li>Desktop</li>
+          <li>Cloud</li>
+        </ul>
+        <div className="subscribe">
+          <h3>Subscribe</h3>
+          <p>Enter your email to get notified about our news solutions</p>
+          <div className="input">
+            <input type="email" />
+            <button>
+              <span className="material-symbols-outlined">mail</span>
+            </button>
+          </div>
         </div>
-        <div className="footerRight">
-          Mọi trận đấu cho dù giải nhỏ cho tới các giải đấu lớn thì FOOTBALL đều
-          cung cấp đầy đủ link xem trực tiếp bóng đá online với độ phân giải và
-          chất lượng cao nhất. Ngoài việc xem bóng đá trực tuyến, chúng tôi còn
-          gửi tới bạn đọc lịch thi đấu bóng đá, kết quả bóng đá và soi kèo bóng
-          đá với tỷ lệ chiến thắng cao. Chúc bạn đọc xem bóng đá vui vẻ và luôn
-          ủng hộ FOOTBALL
-        </div>
-      </FooterStyle>
-    </div>
+      </div>
+    </FooterStyle>
   );
 };
 
